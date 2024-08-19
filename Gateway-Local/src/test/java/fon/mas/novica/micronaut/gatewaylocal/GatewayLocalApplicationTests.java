3f76418782c1dca361910235d067ced25f8e4967
Novke <novke.brate@gmail.com>
2024-08-19 12:43:42 +0200

    gateway-local init

---
+++ Gateway-Local/src/test/java/fon/mas/novica/micronaut/gatewaylocal/GatewayLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package fon.mas.novica.micronaut.gatewaylocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayLocalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
