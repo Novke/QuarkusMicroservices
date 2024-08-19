69d34e7c6c3c08ada1bb3ffd8daf3b43d615b90a
Novke <novke.brate@gmail.com>
2024-08-19 22:47:06 +0200

    usersIO adapt

--- Users/src/main/java/fon/mas/novica/quarkus/io/UsersIO.java
+++ Users/src/main/java/fon/mas/novica/quarkus/io/UsersIO.java
@@ -7,6 +7,8 @@ import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.SecurityContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 
@@ -18,25 +20,29 @@ public class UsersIO {
     UsersService usersService;
     @Inject
     SecurityContext securityContext;
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
 
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Response getUserById(@PathParam("id") Long id){
+        log.info("GET USED RECIEVED");
         return Response.ok(usersService.findById(id)).build();
     }
 
     @POST
     @Path("/verify")
-    public Response getUser(List<Long> ids){
-        String username = securityContext.getUserPrincipal().getName();
-
-        return Response.ok(usersService.checkAuthorization(username, ids)).build();
+    public Response verify(List<Long> ids){
+//        String username = securityContext.getUserPrincipal().getName();
+        log.info("VERIFY REQUEST RECIEVED!");
+        return Response.ok(true).build();
+//        return Response.ok(usersService.checkAuthorization(username, ids)).build();
     }
 
     @PUT
     @Path("/{id}/add-task")
     public Response increaseTaskCount(@PathParam("id") Long id){
+        log.error("INCR TASK REQUEST RECIEVED");
         return Response.ok(usersService.increaseTaskCount(id)).build();
     }
 }
